4d0b26aa0769d1c6ae3dbfb5df13a6940e0d1647
Saurabh Kumar <skumar9876@gmail.com>
2018-01-04 14:29:43 -0600

    Added controller ordering to environment state.

--- envs/scheduling_env.py
+++ envs/scheduling_env.py
@@ -13,23 +13,34 @@ class EventScheduling(object):
 		   Args:
 		   	num_agents: the number of events that need to be scheduled.
 		   	num_times: the number of possible times for each event.
-
-		   Returns:
-		    databases: dictionary containing each controller's database.
 		"""
 		self.num_agents = num_agents
 		self.num_times = num_times
 
-		times = np.arange(self.num_times)
-		# Generate a possible output ordering. 
-		self.possible_ordering = np.sort(random.sample(times, self.num_agents))
-		# Based on this ordering, construct the controllers' databases.
+	def reset(self):
+		"""Resets the environment.
 
-		self.databases = {}
+		   Returns:
+		   	database_arr: array containing each controller's database.
+		   	ordering: ordering of controllers.
+		"""
+		times = np.arange(self.num_times)
+		# Generate a possible output ordering of times. 
+		possible_ordering = np.sort(random.sample(times, self.num_agents))
+		# Generate a controller ordering.
+		self.controller_ordering = np.arange(self._num_agents)
+		np.random.shuffle(self.controller_ordering)
+
+		# Based on the controller ordering and possible times output,
+		# construct the controllers' databases.
+		self.databases = np.zeros(self.num_times * self.num_agents)
+		self.databases_dict = {}
 		for i in xrange(self.num_agents):
+			curr_controller = self.controller_ordering[i]
+
 			# Each database is a multi-hot vector indicating the available times
 			database = np.zeros(self.num_times)
-			database[self.possible_ordering[i]] = 1
+			database[self.possible_ordering[curr_controller]] = 1
 
 			# Set random subset of entries in the database to 1.
 			subset_size = random.randint(0, self.num_times - 1)
@@ -37,12 +48,13 @@ class EventScheduling(object):
 				all_times = np.arange(self.num_times)
 				database[random.sample(all_times, subset_size)] = 1
 
-			self.databases[i] = np.copy(database)
+			self.databases_dict[self.curr_controller] = np.copy(database)
+			self.databases[self.num_times * self.curr_controller : self.num_times * (
+				curr_controller + 1)] = np.copy(database)
 
-	def get_databases(self):
-		return self.databases
- 
-	def score(output_schedule):
+		return np.copy(self.databases), np.copy(self.controller_ordering)
+	
+	def score(self, output_schedule):
 		"""Returns the score for a given output schedule. 
 
 			Args:
@@ -52,14 +64,26 @@ class EventScheduling(object):
 			 score: +1 if schedule is valid, -1 if invalid.
 		"""
 
-		# Checks that the output schedule is in strictly increasing order 
-		# and that each output time is in the corresponding controller's
-		# databse.
+		# Checks whether output schedule is complete or incomplete. 
+		# Output schedule is incomplete if its length is not equal to the number of agents.
+		# If output schedule is incomplete, returns 0.
+		if np.size(output_schedule) < self.num_agents:
+			return 0
+
+		# Checks that the output schedule is in strictly increasing order
+		# according to the controller ordering and that each output time
+		# is in the corresponding controller's database.
+		val = output_schedule[self.controller_ordering[0]]
+
 		for i in xrange(len(output_schedule)):
-			val = output_schedule[i]
-			if val > output_schedule[i] or (
-				val >= 1 and val >= output_schedule[i]):
-				return -1
-			elif not self.databases[i][val]:
-					return -1
+			controller_index = self.controller_ordering[i]
+
+			if val > output_schedule[controller_index] or (
+				i >= 1 and val >= output_schedule[controller_index]):
+				return 0
+
+			elif not self.databases[controller_index][output_schedule[controller_index]]:
+					return 0
+
+			val = output_schedule[controller_index]
 		return 1
